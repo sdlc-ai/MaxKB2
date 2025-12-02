d25c047216d82e8f9ade2f0dc0d3676aab6b9f61
wzl <490953935@qq.com>
2025-12-02 22:51:24 +0800

    refactor: build in local env.

---
+++ installer/run-build-sdlc-base-image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ../
+docker rmi -f sdlc-base:python3.11-pg17.6
+docker build -f installer/Dockerfile-base -t sdlc-base:python3.11-pg17.6 .
+docker rmi -f sdlc-vector-model:v1.0.1
+docker build -f installer/Dockerfile-vector-model -t sdlc-vector-model:v1.0.1 .
