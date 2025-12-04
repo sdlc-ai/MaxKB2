48162dd4a5f278292ad21cee4ee441518a9cd3f4
wzl <490953935@qq.com>
2025-12-04 22:18:41 +0800

    refactor: remove useless code.

--- installer/run-build-sdlc-base-image.sh
+++ installer/run-build-sdlc-base-image.sh
@@ -4,3 +4,5 @@ docker rmi -f porsche-base:python3.11-pg17.6
 docker build -f installer/Dockerfile-base -t porsche-base:python3.11-pg17.6 .
 docker rmi -f porsche-vector-model:v1.0.1
 docker build -f installer/Dockerfile-vector-model -t porsche-vector-model:v1.0.1 .
+
+# docker build -f installer/Dockerfile-base -t porsche-base:python3.11-pg17.6 .
