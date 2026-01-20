10a69a87a1418833336ce34d39d43ed9e8ed4c00
Lu Yilin <rick.ren@porsche.digital>
2026-01-20 15:14:47 +0800

    Revert "feat: add prosche"
    This reverts commit 57dc4362

--- apps/porsche/settings/auth/model.py
+++
@@ -1,4 +0,0 @@
-# coding=utf-8
-
-AUTH_HANDLES = [
-]
--- apps/porsche/settings/base/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-import os
-
-if os.environ.get('SERVER_NAME', 'web') == 'local_model':
-    from .model import *
-else:
-    from .web import *
