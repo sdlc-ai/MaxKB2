ff3302944d0b95fd23fdc351ba5769490d744443
liuyuan <yuan.a.liu@capgemini.com>
2026-04-14 09:57:03 +0800

    fix

--- installer/run-build-sdlc-base-image.sh
+++ installer/run-build-sdlc-base-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../
-docker docker rmi -f sdlc-base:python3.11-pg17.6
-docker docker buildx build --platform linux/amd64 -f installer/Dockerfile-base -t sdlc-base:python3.11-pg17.6 .
-docker rmi -f porsche-vector-model:v1.0.1
-docker build -f installer/Dockerfile-vector-model -t porsche-vector-model:v1.0.1 .
+docker rmi -f sdlc-base:python3.11-pg17.6
+docker buildx build --platform linux/amd64 -f installer/Dockerfile-base -t sdlc-base:python3.11-pg17.6 .
+docker rmi -f sdlc-vector-model:v1.0.1
+docker build -f installer/Dockerfile-vector-model -t sdlc-vector-model:v1.0.1 .
